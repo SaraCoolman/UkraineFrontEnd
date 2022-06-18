7dd22dfded47c3b618c4450e866f0157d9fb1a53
SaraCoolman <56956632+SaraCoolman@users.noreply.github.com>
2022-06-18 10:55:04 +0200

    url geändert

---
+++ build/precache-manifest.4f24d7e66fd0b8a94c1419ae08052d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9f738fa42d546aa41b4",
+    "url": "/static/css/main.3fb0d24b.chunk.css"
+  },
+  {
+    "revision": "d9f738fa42d546aa41b4",
+    "url": "/static/js/main.d9f738fa.chunk.js"
+  },
+  {
+    "revision": "05c92a787469c43038e6",
+    "url": "/static/css/1.e6f2b090.chunk.css"
+  },
+  {
+    "revision": "05c92a787469c43038e6",
+    "url": "/static/js/1.05c92a78.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "97a813c86641e323e5257f22cce1663a",
+    "url": "/index.html"
+  }
+];
